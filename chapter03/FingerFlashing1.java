fcbb5775b55a0644da7ad67d25b40db6182c0632
yshura <108316787+yshura@users.noreply.github.com>
2022-11-09 03:53:54 +0000

    swich文

---
+++ chapter03/FingerFlashing1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class FingerFlashing1 {
+  public static void main(String[] args) {
+    Scanner stdIn = new Scanner(System.in);
+    
+    System.out.print("手を選べ (0・・・グー/1・・・チョキ/2・・・パー) :");
+    int hand = stdIn.nextInt();
+    
+    // if (hand == 0)
+    //   System.out.println("グー");
+    // else if (hand == 1)
+    //   System.out.println("チョキ");
+    // else if (hand == 2)
+    //   System.out.println("パー");
+    
+    switch (hand) {
+      case 0 : System.out.println("グー"); break;
+      case 1 : System.out.println("チョキ"); break;
+      case 2 : System.out.println("パー"); break
+    }
+  }
+}
