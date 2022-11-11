119bb40323ccb4f8d4022d797480adc0f5c57d8c
yshura <108316787+yshura@users.noreply.github.com>
2022-11-11 13:02:39 +0000

    do文

---
+++ chapter04/Season.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Season {
+  public static void main(String[] args) {
+    Scanner stdIn = new Scanner(System.in);
+    
+    int retry;
+    
+    do {
+      System.out.print("季節を求めます。 \n何月ですか？：");
+      int month = stdIn.nextInt();
+      
+      if (month >= 3 && month <= 5)
+        System.out.println("それは春です。");
+      else if (month >= 6 && month <= 8)
+        System.out.println("それは夏です。");
+      else if (month >= 9 && month <= 11)
+        System.out.println("それは秋です。");
+      else if (month == 12 || month == 1 || month == 2)
+        System.out.println("それは冬です。");
+        
+      System.out.print("もう一度？ 1・・・Yes/0・・・No：");
+      retry = stdIn.nextInt();
+    } while (retry == 1);
+  }
+}
