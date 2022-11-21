e71b4cdc21271e6f037dee0806be2ed588d04cfb
yshura <108316787+yshura@users.noreply.github.com>
2022-11-21 04:30:09 +0000

    ラベル文

--- chapter04/SumGroup1.java
+++ chapter04/SumGroup1.java
@@ -10,19 +10,16 @@ class SumGroup1 {
   Outer:
     for (int i = 1; i <= 10; i++) {
       System.out.println("■第" + i + "グループ");
-      int sum = 0;
-    Inner:
+    
       for (int j = 0; j < 5; j++) {
         System.out.print("整数：");
         int t = stdIn.nextInt();
         if (t == 99999)
           break Outer;
         else if (t == 88888)
-          break Inner;
-        sum += t;
+          continue Outer;
+        total += t;
       }
-      System.out.println("小計は" + sum + "です。");
-      total += sum;
     }
     System.out.println("\n合計は" + total + "です。");
   }
